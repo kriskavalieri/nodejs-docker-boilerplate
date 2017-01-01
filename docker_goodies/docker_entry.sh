0886cff8ef9a5b20a788c879d3b28ca15d02e877
Kris Kavalieri <kris.kavalieri@gmail.com>
2017-01-02 00:06:37 +0100

    Edit istanbul version

--- docker_goodies/docker_entry.sh
+++ docker_goodies/docker_entry.sh
@@ -3,7 +3,7 @@
 grep -cm1 docker /proc/1/cgroup 2> /dev/null || echo "* Cannot execute outside Docker" && exit 1
 echo "* Running coverage checks..."
 rm -rf .cov_temp
-babel-node ./node_modules/istanbul/lib/cli.js cover --config .istanbul-config.yml _mocha -- src --recursive
+babel-node ./node_modules/istanbul/lib/cli.js cover --config .istanbul-config.yml ./node_modules/mocha/bin/_mocha -- --recursive
 istanbul check-coverage --root .cov_temp --config .istanbul-config.yml &> .cov_temp/coverage-summary
 
 echo "* Running lint checks..."
