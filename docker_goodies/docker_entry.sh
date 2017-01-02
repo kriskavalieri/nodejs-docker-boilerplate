019712e6cfe9a7cfcbf5f80edb7c41726fba0376
Kris Kavalieri <kris.kavalieri@gmail.com>
2017-01-02 14:45:55 +0100

    Absolutize paths

--- docker_goodies/docker_entry.sh
+++ docker_goodies/docker_entry.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # quick self-identity check
-grep -cm1 docker /proc/1/cgroup 2> /dev/null || echo "* Cannot execute outside Docker" && exit 1
+[[ `grep -cm1 docker /proc/1/cgroup 2> /dev/null` ]] || { echo "* Cannot execute outside Docker"; exit 1; }
 echo "* Running coverage checks..."
 rm -rf .cov_temp
-babel-node ./node_modules/istanbul/lib/cli.js cover --config .istanbul-config.yml ./node_modules/mocha/bin/_mocha -- --recursive
-istanbul check-coverage --root .cov_temp --config .istanbul-config.yml &> .cov_temp/coverage-summary
+./node_modules/babel-cli/bin/babel-node.js \
+   ./node_modules/istanbul/lib/cli.js cover \
+   --config .istanbul-config.yml \
+   ./node_modules/mocha/bin/_mocha -- --recursive
+
+./node_modules/istanbul/lib/cli.js check-coverage --root .cov_temp --config .istanbul-config.yml &> .cov_temp/coverage-summary
 
 echo "* Running lint checks..."
 cat /dev/null > .lint_out
-eslint -f compact -o .lint_out .
+./node_modules/eslint/bin/eslint.js -f compact -o .lint_out .
