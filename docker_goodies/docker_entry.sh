af22948a52f5581b2f0952514dd83c52f4e56555
Kris Kavalieri <kris.kavalieri@gmail.com>
2017-01-02 14:56:50 +0100

    Source node_modules from /tmp

--- docker_goodies/docker_entry.sh
+++ docker_goodies/docker_entry.sh
@@ -3,13 +3,14 @@
 [[ `grep -cm1 docker /proc/1/cgroup 2> /dev/null` ]] || { echo "* Cannot execute outside Docker"; exit 1; }
 echo "* Running coverage checks..."
 rm -rf .cov_temp
-./node_modules/babel-cli/bin/babel-node.js \
-   ./node_modules/istanbul/lib/cli.js cover \
+NODE_PATH="/tmp/node_modules"
+$NODE_PATH/babel-cli/bin/babel-node.js \
+   $NODE_PATH/istanbul/lib/cli.js cover \
    --config .istanbul-config.yml \
-   ./node_modules/mocha/bin/_mocha -- --recursive
+   $NODE_PATH/mocha/bin/_mocha -- --recursive
 
-./node_modules/istanbul/lib/cli.js check-coverage --root .cov_temp --config .istanbul-config.yml &> .cov_temp/coverage-summary
+$NODE_PATH/istanbul/lib/cli.js check-coverage --root .cov_temp --config .istanbul-config.yml &> .cov_temp/coverage-summary
 
 echo "* Running lint checks..."
 cat /dev/null > .lint_out
-./node_modules/eslint/bin/eslint.js -f compact -o .lint_out .
+$NODE_PATH/eslint/bin/eslint.js -f compact -o .lint_out .
