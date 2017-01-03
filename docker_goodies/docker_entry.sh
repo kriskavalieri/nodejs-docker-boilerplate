fb1e09eb8fd7b51ae4b71985261e06103e7569c5
Kris Kavalieri <kris.kavalieri@gmail.com>
2017-01-03 13:49:38 +0100

    create .lint_out anyway

--- docker_goodies/docker_entry.sh
+++ docker_goodies/docker_entry.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # quick self-identity check
-[[ `grep -cm1 docker /proc/1/cgroup 2> /dev/null` ]] || { echo "* Cannot execute outside Docker"; exit 1; }
+[[ `grep -cm1 docker /proc/1/cgroup 2> /dev/null` ]] || { echo "* Cannot execute outside Docker scope"; exit 1; }
 echo -e "\t+ Running coverage checks..."
 rm -rf .cov_temp
+cat /dev/null > .lint_out
 NODE_MODULES_PATH="/tmp/node_modules"
 $NODE_MODULES_PATH/babel-cli/bin/babel-node.js \
    $NODE_MODULES_PATH/istanbul/lib/cli.js cover \
    --config .istanbul-config.yml \
    $NODE_MODULES_PATH/mocha/bin/_mocha -- --recursive &> /dev/null
 
-$NODE_MODULES_PATH/istanbul/lib/cli.js check-coverage --root .cov_temp --config .istanbul-config.yml &> .cov_temp/coverage-summary
+$NODE_MODULES_PATH/istanbul/lib/cli.js \
+   check-coverage --root .cov_temp \
+   --config .istanbul-config.yml &> \
+   .cov_temp/coverage-summary
 
 if [ -e .eslintrc ]; then
    echo -e "\t+ Running lint checks..."
-   cat /dev/null > .lint_out
    $NODE_MODULES_PATH/eslint/bin/eslint.js -f compact -o .lint_out .
 fi
