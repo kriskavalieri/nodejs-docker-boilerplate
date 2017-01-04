22e333804cb51dae1c9231210266f237cf8d924c
Kris Kavalieri <kris.kavalieri@gmail.com>
2017-01-04 15:25:33 +0100

    _

--- docker_goodies/docker_entry.sh
+++ docker_goodies/docker_entry.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 # quick self-identity check
-[[ `grep -cm1 docker /proc/1/cgroup 2> /dev/null` ]] || { echo "* Cannot execute outside Docker scope"; exit 1; }
-echo -e "\t+ Running coverage checks..."
-rm -rf .cov_temp
-cat /dev/null > .lint_out
+ERROR='\033[0;31m'
+WARN='\033[0;33m'
+INFO='\033[0;32m'
+EC='\033[0m'
+[[ `grep -cm1 docker /proc/1/cgroup 2> /dev/null` ]] || \
+   { echo -e "${ERROR}* Cannot execute outside Docker scope${EC}"; exit 1; }
 NODE_MODULES_PATH="/tmp/node_modules"
-$NODE_MODULES_PATH/babel-cli/bin/babel-node.js \
-   $NODE_MODULES_PATH/istanbul/lib/cli.js cover \
-   --config .istanbul-config.yml \
-   $NODE_MODULES_PATH/mocha/bin/_mocha -- --recursive &> /dev/null
+cat /dev/null | tee .lint_out .test_out .test_err
 
-$NODE_MODULES_PATH/istanbul/lib/cli.js \
-   check-coverage --root .cov_temp \
-   --config .istanbul-config.yml &> \
-   .cov_temp/coverage-summary
+echo -e "\t${INFO}+ Running tests...${EC}"
+npm run test 2> .test_err 1> .test_out
+[[ "$?" -gt 0 ]] && cat /dev/null > .test_err
+
+# echo -e "\t${INFO}+ Running coverage checks...${EC}"
+# rm -rf .cov_temp
+# $NODE_MODULES_PATH/babel-cli/bin/babel-node.js \
+#    $NODE_MODULES_PATH/istanbul/lib/cli.js cover \
+#    --config .istanbul-config.yml \
+#    $NODE_MODULES_PATH/mocha/bin/_mocha -- --recursive &> /dev/null
+# $NODE_MODULES_PATH/istanbul/lib/cli.js \
+#    check-coverage --root .cov_temp \
+#    --config .istanbul-config.yml &> \
+#    .cov_temp/coverage-summary
+
 
 if [ -e .eslintrc ]; then
-   echo -e "\t+ Running lint checks..."
-   $NODE_MODULES_PATH/eslint/bin/eslint.js -f compact -o .lint_out .
+   echo -e "\t${INFO}+ Running lint checks...${EC}"
+   $NODE_MODULES_PATH/eslint/bin/eslint.js -f compact -o .lint_out
+else
+   echo -e "\t${WARN}+ Lint checks skipped, no .eslintrc present.${EC}"
 fi
--- pre_push.py
+++ pre_push.py
@@ -28,29 +28,42 @@ def set_mode_prefixes():
     cov_34_mode = ''
     current_branch = get_current_branch()
     if not stable_branch_re.search(current_branch):
-        prefix = "* On branch {} which is likely not production-bound, so executing low-threshold checks...".format(current_branch)
+        prefix = "* On branch {} which is likely not production-bound, executing nondiscriminatory checks...".format(current_branch)
         cov_34_mode = "_light"
     else:
         prefix = "* Push to production branch detected, executing full-scale checks..."
     print(prefix + "\n* Running pre-flight checks, please hold...")
     return cov_34_mode
 
-def run_checks(GIT_ROOT, verbose=False):
+def run_checks(GIT_ROOT, verbose=False, devmode=False):
     test_dir = glob.glob(GIT_ROOT + "/test*")
     assert test_dir and os.path.isdir(test_dir[0]), \
            "Package's test directory not found"
     COV_SUMMARY = os.path.join(GIT_ROOT, ".cov_temp/coverage-summary")
     LINT_OUT = os.path.join(GIT_ROOT, ".lint_out")
+    TEST_OUT = os.path.join(GIT_ROOT, ".test_out")
+    TEST_ERR = os.path.join(GIT_ROOT, ".test_err")
     os.chdir(GIT_ROOT)
     args = " -v" if verbose else ""
     os.system("bash {}/run_checks_in_docker.sh {}".format(GIT_ROOT, args))
     out = ""
-    with open(COV_SUMMARY) as cov_summary, open(LINT_OUT) as lint:
-        # on account of insignificantly low no of lines of .lint_out,
-        # let's just read the whole thing at once
+    with open(COV_SUMMARY) as cov_summary, \
+         open(LINT_OUT) as lint, \
+         open(TEST_ERR) as test_err, \
+         open(TEST_OUT) as test_out:
+
+        if os.path.getsize(TEST_ERR):
+            out += ("\n* Tests failed. Error output was: \n{}\n"
+                    "Test tool ouput was: \n{}\n").format(
+                        test_err.read().strip(),
+                        test_out.read().strip()
+                    )
+
         if os.path.getsize(LINT_OUT):
             # file not empty, errors/warnings present, interrupt and print
-            out += "\n* Lint returned some errors/warnings: \n{}\n\n".format(lint.read().strip())
+            out += "\n* Lint returned some errors/warnings: \n{}\n\n".format(
+                lint.read().strip() if not devmode else subprocess.check_output(['tail', '-1', LINT_OUT])
+            )
         total_covs = cov_summary.read()
         if total_covs:
             istanbul_rates = json.dumps(
@@ -58,10 +71,11 @@ def run_checks(GIT_ROOT, verbose=False):
             out += "* Istanbul complained about too low coverage rates: \n{}".format(istanbul_rates)
     separator = "\n{:#^70}\n".format(" Summary ")
     print(separator)
-    if out:
+
+    if out and not devmode:
         print(out)
         sys.exit(1)
-    print("* All checked out good")
+    print("* All clear")
 
 if __name__ == "__main__":
     cov_34_mode = set_mode_prefixes()
@@ -73,4 +87,4 @@ if __name__ == "__main__":
     def restore_default_34_config():
         os.system("cp {} ../.istanbul-config.yml".format(
             COV_34_CONFIG.replace(cov_34_mode, "")))
-    run_checks(GIT_ROOT)
+    run_checks(GIT_ROOT, devmode=bool(cov_34_mode))
